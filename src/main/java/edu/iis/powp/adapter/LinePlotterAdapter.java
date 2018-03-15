8f0772dcc43594ae71132a3d50ec04af888e10d1
szejn1337 <202289@edu.p.lodz.pl>
2018-03-15 14:47:53 +0100

    “Uzupelnienie”

---
+++ src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
@@ -0,0 +1,53 @@
+package edu.iis.powp.adapter;
+
+import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.appext.ApplicationWithDrawer;
+import edu.kis.powp.drawer.panel.DrawPanelController;
+import edu.kis.powp.drawer.shape.ILine;
+import edu.kis.powp.drawer.shape.LineFactory;
+
+
+/**
+ * Plotter adapter to drawer with several bugs. 
+ */
+public class LinePlotterAdapter extends DrawPanelController implements IPlotter
+{ 
+	private int startX = 0, startY = 0;
+	
+    public LinePlotterAdapter() {
+		super();
+	}
+    
+    
+	@Override
+    public void setPosition(int x, int y)
+    {
+        this.startX = x;
+        this.startY = y;
+    }
+
+	
+    @Override
+    public void drawTo(int x, int y)
+    {
+        ILine line = LineFactory.getSpecialLine();
+    	line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+        
+        setPosition(x, y);
+        
+		ApplicationWithDrawer.getDrawPanelController().drawLine(line);
+		
+		//drawLine(line);
+		
+
+
+    }
+
+    
+    @Override
+    public String toString()
+    {
+        return "LinePlotterAdapter";
+    }
+}
