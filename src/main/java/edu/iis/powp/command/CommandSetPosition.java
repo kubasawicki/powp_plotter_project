c38a6c0d1b6d4605c44547932bb8a894e43285a5
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-03-15 15:09:38 +0100

    Added classes CommandSetPosition.java and CommandDrawLineToPosition.java and ComplexCommand.java

---
+++ src/main/java/edu/iis/powp/command/CommandSetPosition.java
@@ -0,0 +1,16 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class CommandSetPosition implements PlotterCommand {
+	
+	private int x;
+	private int y;
+
+	@Override
+	public void execute(IPlotter plotter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -0,0 +1,13 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class ComplexCommand implements PlotterCommand {
+
+	@Override
+	public void execute(IPlotter plotter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
