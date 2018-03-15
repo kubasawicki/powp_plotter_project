8b1f17a3e0c2fc36c2d8c61733caeecdb591f966
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-03-15 15:33:22 +0100

    Updated ComplexCommand.java class

--- src/main/java/edu/iis/powp/command/ComplexCommand.java
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -1,12 +1,21 @@
 package edu.iis.powp.command;
 
+import java.util.List;
+
 import edu.iis.client.plottermagic.IPlotter;
 
 public class ComplexCommand implements PlotterCommand {
+	
+	private List<PlotterCommand>ListOfCommands;
+	
+	public ComplexCommand(List<PlotterCommand> commands) {
+		ListOfCommands = commands;
+	}
 
 	@Override
 	public void execute(IPlotter plotter) {
-		// TODO Auto-generated method stub
+
+		
 		
 	}
 
